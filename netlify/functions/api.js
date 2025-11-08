7056cc3eb428b4e3982bb4f009389e3bf7c2a03d
tfeng <tn388816@dal.ca>
2025-11-07 23:28:46 -0400

    test

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const fs = require("fs");
 const serverless = require("serverless-http");
-const fetch = require("node-fetch");
+
 
 const app = express();
 
